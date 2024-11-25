890084b3c52c7f323d1fdc73f6e7751527fa6cbf
controls player <130595852+site-codes@users.noreply.github.com>
2024-11-25 03:25:21 -0300

    Update linkspageanime.js

--- PaginaAnime/linkspageanime.js
+++ PaginaAnime/linkspageanime.js
@@ -1,5 +1,5 @@
 document.write(`
-   <link rel="stylesheet" href=" https://instinto-play.web.app/PaginaAnime/style.css">
-   <script src=" https://instinto-play.web.app/PaginaAnime/script.js"><\/script>
-   <script src=" https://instinto-play.web.app/PaginaAnime/listaeps.js"><\/script>
+   <link rel="stylesheet" href="https://instinto-play.vercel.app/PaginaAnime/style.css">
+   <script src="https://instinto-play.vercel.app/PaginaAnime/script.js"><\/script>
+   <script src="https://instinto-play.vercel.app/PaginaAnime/listaeps.js"><\/script>
 `);
