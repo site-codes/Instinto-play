0d0ae26f0efb129d0cd6ffe2cc3d3a6292ec4259
controls player <130595852+site-codes@users.noreply.github.com>
2024-11-25 03:07:01 -0300

    Update scriptLink.js

--- scriptLink.js
+++ scriptLink.js
@@ -1,85 +1,15 @@
-const _0x9e8826 = _0x3411;
-function _0x4cf8() {
-    const _0x1d245a = [
-        'stintoanim',
-        'tps://inst',
-        'undefined',
-        'write',
-        'nar.\x20Acess',
-        'pp/site.js',
-        '11jSuUKI',
-        '<script\x20sr',
-        'não\x20possui',
-        '2227460XQFhcj',
-        '\x20licença\x20p',
-        'includes',
-        'https://in',
-        '35DLTkFv',
-        '9762mFdOFK',
-        'location',
-        '274740ZVWShD',
-        'href',
-        '12MnfTcw',
-        'chavekEY',
-        'intoanimes',
-        'querySelec',
-        '934380PLRvew',
-        'c=\x22https:/',
-        'Esse\x20site\x20',
-        't.com/',
-        'script',
-        '518192ajnLLg',
-        'ara\x20funcio',
-        'com/',
-        'torAll',
-        '12kpaFEi',
-        'ficial:\x20ht',
-        'remove',
-        'e\x20o\x20link\x20o',
-        '126232dhUsZX',
-        'play.web.a',
-        '.blogspot.',
-        'forEach',
-        'textConten',
-        '/instinto-',
-        '\x22></script',
-        '114402VIEZcL',
-        'es.blogspo'
-    ];
-    _0x4cf8 = function () {
-        return _0x1d245a;
-    };
-    return _0x4cf8();
-}
-function _0x3411(_0x25860d, _0x5e1213) {
-    const _0x1661a1 = _0x4cf8();
-    return _0x3411 = function (_0x4249be, _0x3dc0d6) {
-        _0x4249be = _0x4249be - (0x1 * -0x266d + 0x1f03 * 0x1 + 0x13 * 0x6f);
-        let _0x19fb09 = _0x1661a1[_0x4249be];
-        return _0x19fb09;
-    }, _0x3411(_0x25860d, _0x5e1213);
-}
-(function (_0x20c983, _0x1e662b) {
-    const _0x43be85 = _0x3411, _0x52b815 = _0x20c983();
-    while (!![]) {
-        try {
-            const _0x3810e7 = -parseInt(_0x43be85(0xd4)) / (-0x1 * 0x1129 + 0x101 * 0x10 + 0x11a) * (parseInt(_0x43be85(0xfc)) / (-0x1d36 * -0x1 + -0x460 + -0x18d4)) + -parseInt(_0x43be85(0xe1)) / (0x21a0 + 0x1252 + -0x5 * 0xa63) * (-parseInt(_0x43be85(0xdd)) / (0x409 * -0x7 + 0x13e * 0x9 + 0x1115)) + parseInt(_0x43be85(0xfe)) / (-0x17 * 0x5 + -0x1 * 0x14bb + 0x1533) + -parseInt(_0x43be85(0xec)) / (0x9 * -0x3bc + 0x11ed + -0x1 * -0xfb5) * (parseInt(_0x43be85(0xfb)) / (0x26de + 0x919 + -0x3b * 0xd0)) + -parseInt(_0x43be85(0xe5)) / (-0x7f * 0x47 + 0x9 * 0x73 + 0x2f * 0xaa) + parseInt(_0x43be85(0xd8)) / (-0x443 * -0x5 + 0x4ab * 0x1 + -0x19f1) + -parseInt(_0x43be85(0xf7)) / (-0x1b3f + -0xde9 + 0x1499 * 0x2) * (parseInt(_0x43be85(0xf4)) / (0x1566 + 0x5b1 + -0x241 * 0xc));
-            if (_0x3810e7 === _0x1e662b)
-                break;
-            else
-                _0x52b815['push'](_0x52b815['shift']());
-        } catch (_0x566a30) {
-            _0x52b815['push'](_0x52b815['shift']());
-        }
+
+if (typeof window.chavekEY !== 'undefined') {
+  document.write(`<script src="https://instinto-play.vercel.app/site.js"><\/script>`);
+
+} else {
+  alert('Esse site não possui licença para funcionar. Acesse o link oficial: https://instintoanimes.blogspot.com/');
+  window.location.href = 'https://instintoanimes.blogspot.com/'; // Redireciona para o link oficial
+
+const scripts = document.querySelectorAll('script');
+  scripts.forEach(script => {
+    if (script.textContent.includes(linkBase)) {
+      script.remove(); // Remove o script do body
     }
-}(_0x4cf8, 0x2 * -0x1fd47 + -0x2692d * -0x3 + -0x1 * -0x11677));
-if (typeof window[_0x9e8826(0xd5)] !== _0x9e8826(0xf0))
-    document[_0x9e8826(0xf1)](_0x9e8826(0xf5) + _0x9e8826(0xd9) + _0x9e8826(0xea) + _0x9e8826(0xe6) + _0x9e8826(0xf3) + _0x9e8826(0xeb) + '>');
-else {
-    alert(_0x9e8826(0xda) + _0x9e8826(0xf6) + _0x9e8826(0xf8) + _0x9e8826(0xde) + _0x9e8826(0xf2) + _0x9e8826(0xe4) + _0x9e8826(0xe2) + _0x9e8826(0xef) + _0x9e8826(0xd6) + _0x9e8826(0xe7) + _0x9e8826(0xdf)), window[_0x9e8826(0xfd)][_0x9e8826(0xd3)] = _0x9e8826(0xfa) + _0x9e8826(0xee) + _0x9e8826(0xed) + _0x9e8826(0xdb);
-    const scripts = document[_0x9e8826(0xd7) + _0x9e8826(0xe0)](_0x9e8826(0xdc));
-    scripts[_0x9e8826(0xe8)](_0x3571ee => {
-        const _0x5d189a = _0x9e8826;
-        _0x3571ee[_0x5d189a(0xe9) + 't'][_0x5d189a(0xf9)](linkBase) && _0x3571ee[_0x5d189a(0xe3)]();
-    });
+  });
 }
