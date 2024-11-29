a7f9f848bcb561a4821e41916949724ebf24571c
controls player <130595852+site-codes@users.noreply.github.com>
2024-11-29 17:55:46 -0300

    Delete Main-esboços directory

--- Main-esboços/Gerador/js/scroll.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-    const header = document.querySelector("header");
-    if (window.scrollY > 0) {
-        header.classList.add("scroll");
-    } else {
-        header.classList.remove("scroll");
-    }
-});
