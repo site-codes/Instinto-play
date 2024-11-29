480d8aeed2a759ddb7555588b637a0f9b3f16f9b
controls player <130595852+site-codes@users.noreply.github.com>
2024-11-29 17:56:19 -0300

    Add files via upload

---
+++ Main/Gerador/js/pages.js
@@ -0,0 +1,320 @@
+function validateYouTubeLink() {
+    const trailerInput = document.getElementById('trailer');
+    const trailerLinkInput = document.getElementById('trailerLink');
+    const errorMessage = document.getElementById('trailer-error');
+
+    // Regex para validar link do YouTube
+    const youtubeRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
+
+    // Verifica se o link contém "https://youtu.be/" ou é um link válido
+    if (youtubeRegex.test(trailerInput.value)) {
+        const urlParams = new URLSearchParams(new URL(trailerInput.value).search);
+        const videoId = urlParams.get('v') || trailerInput.value.split('/').pop();
+
+        // Define o link embed
+        trailerLinkInput.value = `https://www.youtube.com/embed/${videoId}`;
+        errorMessage.style.display = 'none'; // Oculta a mensagem de erro
+    } else {
+        trailerLinkInput.value = ''; // Limpa o campo se o link for inválido
+        errorMessage.style.display = 'block'; // Mostra a mensagem de erro
+    }
+}
+
+
+
+// generos
+function translateGenres(genres) {
+    return genres.map(genre => genreTranslation[genre] || genre).join(', ').replace(/&/g, ', ');
+}
+
+// capas links
+function getSeasonLinks() {
+    const seasonLinks = document.getElementById('season-links').value.split('\n');
+    return seasonLinks.map(link => link.trim()).filter(link => link); // Remove links vazios
+}
+
+
+// estaçao
+function getSeason(airDate) {
+    const month = new Date(airDate).getMonth() + 1;
+    if (month >= 3 && month <= 5) return 'Primavera';
+    if (month >= 6 && month <= 8) return 'Verão';
+    if (month >= 9 && month <= 11) return 'Outono';
+    return 'Inverno';
+}
+
+// capturar informaçoes
+
+// data mes e ano
+function formatReleaseDate(releaseDate) {
+    const date = new Date(releaseDate);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Adiciona zero à esquerda se necessário
+    return `/${year}/${month}/`;
+}
+// data ano
+function formatReleaseAno(releaseAno) {
+    const date = new Date(releaseAno);
+    const year = date.getFullYear();
+    return `${year}`;
+}
+// data brasil
+function formatDateToBR(dateString) {
+    const [year, month, day] = dateString.split('-'); // Divide a string em partes
+    return `${day}/${month}/${year}`; // Retorna no formato DD/MM/YYYY
+}
+function formatDateToBRShort(dateString) {
+    const date = new Date(dateString + 'T00:00:00'); 
+    const options = { 
+        day: 'numeric', 
+        month: 'short', 
+        year: 'numeric' 
+    };
+    const formattedDate = date.toLocaleDateString('pt-BR', options); // Formata a data no padrão brasileiro
+    return formattedDate; // Mantém o ponto após o mês abreviado
+}
+
+function normalizeTitle(title) {
+    // Remove acentos
+    const normalized = title.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    
+    // Substitui o '&' por 'e' antes de realizar as outras substituições
+    return normalized.toLowerCase()
+        .replace(/&/g, 'e')               // Substitui '&' por 'e'
+        .replace(/\s+/g, '-')             // Substitui espaços por hífens
+        .replace(/[^\w\-]+/g, '')         // Remove caracteres não alfanuméricos e hífens
+        .replace(/-+$/, '');              // Remove hífens no final
+}
+
+
+function updatePageLink() {
+    const releaseYearMonth = formatReleaseDate(document.getElementById('release').value);
+    const title = normalizeTitle(document.getElementById('title').value); // Normaliza o título
+    const subdubValue = document.getElementById('subdub').value.toLowerCase(); // Transforma subdub em minúsculas
+
+    // Adiciona '-dublado' ao pageLink apenas se subdub for 'Dublado'
+    const subdubLink = subdubValue === 'dublado' ? '-dublado' : '';
+    document.getElementById('pageLink').value = `${releaseYearMonth}${title}${subdubLink}`.trim();
+}
+
+
+    // Banner adicojnar
+    document.getElementById("bannerSelect").addEventListener("change", function() {
+        const bannerInput = document.getElementById("banner");
+        if (this.value === "sim") {
+            bannerInput.value = "Banner,";
+        } else {
+            bannerInput.value = "";
+        }
+    });
+
+
+
+    // copiar code
+    function copiarTexto() {
+        const texto = document.getElementById("exibir-pre").innerText;
+        navigator.clipboard.writeText(texto)
+            .then(() => {
+                alert("Texto copiado com sucesso!");
+            })
+            .catch(err => {
+                console.error("Erro ao copiar o texto: ", err);
+            });
+    }
+
+
+    // Função chamada ao clicar em uma imagem
+    function selectImage(imgElement, inputId) {
+        // Atribui o src da imagem selecionada ao input correspondente
+        const inputField = document.getElementById(inputId);
+        inputField.value = imgElement.src;
+
+        // Atualiza a imagem de visualização na âncora
+        const previewImage = inputField.closest('.input-group').querySelector('.imagePreview');
+        previewImage.src = imgElement.src;
+
+        // Atualiza o link da imagem associada
+        const linkElement = previewImage.parentElement;
+        linkElement.href = imgElement.src;
+
+        // Atualiza a imagem
+        updateImage(inputField);
+    }
+
+    // Função para atualizar a imagem
+    function updateImage(input) {
+        const imageUrl = input.value; // Pega o valor do input
+        const imageElement = input.closest('.input-group').querySelector('.imagePreview'); // Encontra a imagem associada
+        const linkElement = imageElement.parentElement;
+
+        if (imageUrl) {
+            imageElement.src = imageUrl; // Atualiza o src da imagem
+            imageElement.style.display = 'flex'; // Exibe a imagem
+            linkElement.href = imageUrl; // Atualiza o link da imagem
+        } else {
+            imageElement.style.display = 'none'; // Oculta a imagem
+            linkElement.removeAttribute('href'); // Remove o link da imagem
+        }
+    }
+
+
+window.addEventListener('load', () => {
+    const inputs = document.querySelectorAll('.linkcapa'); // Seleciona todos os inputs com a classe linkcapa
+    inputs.forEach(input => {
+        const imageElement = input.closest('.input-group').querySelector('.imagePreview'); // Seleciona a imagem correspondente
+        const imageUrl = input.value; // Pega o valor do input
+        if (imageUrl) {
+            imageElement.src = imageUrl;  // Atualiza o src da imagem
+            imageElement.style.display = 'flex'; // Exibe a imagem
+            imageElement.parentElement.href = imageUrl; // Atualiza o link da imagem
+        }
+    });
+});
+
+
+document.getElementById('release').addEventListener('input', updatePageLink);
+document.getElementById('title').addEventListener('input', updatePageLink);
+document.getElementById('subdub').addEventListener('change', updatePageLink);
+
+
+
+
+
+
+// GENEROS
+// Função para verificar categorias
+function checkCategories() {
+    const categoryInput = document.getElementById('selectedCategories');  // Pega o valor do input de categorias
+    const categoryValues = categoryInput.value.toLowerCase().split(',').map(val => val.trim()); // Separa os valores digitados no input
+
+    // Pega todos os labels dentro do container com id "categoria"
+    const categoryLabels = document.querySelectorAll('#categoria label');
+
+    categoryLabels.forEach(label => {
+        const checkbox = label.querySelector('input[type="checkbox"]'); // Pega o checkbox dentro do label
+        const checkboxValue = checkbox.value.toLowerCase(); // Valor do checkbox (em minúsculo para garantir a comparação correta)
+
+        // Verifica se o valor do checkbox está na lista de valores digitados
+        if (categoryValues.includes(checkboxValue)) {
+            label.classList.add('check');  // Adiciona a classe .check ao label
+            checkbox.checked = true; // Marca o checkbox
+        } else {
+            label.classList.remove('check');  // Remove a classe .check do label
+            checkbox.checked = false; // Desmarca o checkbox
+        }
+    });
+}
+
+// Função para atualizar categorias no campo de texto
+function updateCategories(event) {
+    const checkbox = event.target;
+    const categoryInput = document.getElementById('selectedCategories');
+    let currentValues = categoryInput.value ? categoryInput.value.split(',').map(val => val.trim()) : [];  // Verifica se o campo já possui valor
+
+    const checkboxValue = checkbox.value.trim();
+    const label = checkbox.closest('label');
+
+    if (checkbox.checked) {
+        // Se for o primeiro item, simplesmente coloca o valor
+        if (currentValues.length === 0) {
+            categoryInput.value = checkboxValue;
+        } else {
+            // Se já houver valores, adiciona o valor com vírgula
+            categoryInput.value = currentValues.join(', ') + ', ' + checkboxValue;
+        }
+        label.classList.add('check');
+    } else {
+        const index = currentValues.indexOf(checkboxValue);
+        if (index !== -1) {
+            currentValues.splice(index, 1);
+            categoryInput.value = currentValues.join(', ');
+        }
+        label.classList.remove('check');
+    }
+}
+
+
+// Adiciona ouvinte de evento para monitorar mudanças nos checkboxes de categorias
+const categoryCheckboxes = document.querySelectorAll('#categoria input[type="checkbox"]');
+categoryCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', updateCategories); // Detecta quando o checkbox de categorias for alterado
+});
+
+// Adiciona ouvinte de evento para monitorar mudanças no input de categorias
+document.getElementById('selectedCategories').addEventListener('input', checkCategories);
+
+// Função para verificar gêneros
+function checkGenres() {
+    const genreInput = document.getElementById('genres');  // Pega o valor do input de gêneros
+    const genreValues = genreInput.value.toLowerCase().split(',').map(val => val.trim()); // Separa os valores digitados no input
+
+    // Pega todos os labels dentro do container com id "genero"
+    const genreLabels = document.querySelectorAll('#genero label');
+
+    genreLabels.forEach(label => {
+        const checkbox = label.querySelector('input[type="checkbox"]'); // Pega o checkbox dentro do label
+        const checkboxValue = checkbox.value.toLowerCase(); // Valor do checkbox (em minúsculo para garantir a comparação correta)
+
+        // Verifica se o valor do checkbox está na lista de valores digitados
+        if (genreValues.includes(checkboxValue)) {
+            label.classList.add('check');  // Adiciona a classe .check ao label
+            checkbox.checked = true; // Marca o checkbox
+        } else {
+            label.classList.remove('check');  // Remove a classe .check do label
+            checkbox.checked = false; // Desmarca o checkbox
+        }
+    });
+}
+
+// Função para atualizar gêneros no campo de texto
+function updateGenres(event) {
+    const checkbox = event.target; // O checkbox que foi clicado
+    const genreInput = document.getElementById('genres');  // Pega o input de gêneros
+    let currentValues = genreInput.value.split(',').map(val => val.trim()); // Pega os valores atuais no input
+
+    const checkboxValue = checkbox.value.trim();
+    const label = checkbox.closest('label');  // Valor do checkbox
+
+    if (checkbox.checked) {
+        // Se o checkbox for marcado e o valor não estiver no input, adiciona
+        if (!currentValues.includes(checkboxValue)) {
+            currentValues.push(checkboxValue); // Adiciona o valor ao array
+            genreInput.value = currentValues.join(', ');  // Atualiza o input com os novos valores
+        }
+        label.classList.add('check');
+    } else {
+        // Se o checkbox for desmarcado, remove o valor
+        const index = currentValues.indexOf(checkboxValue);
+        if (index !== -1) {
+            currentValues.splice(index, 1); // Remove o valor
+            genreInput.value = currentValues.join(', ');  // Atualiza o input com os novos valores
+        }
+        label.classList.remove('check');
+    }
+}
+
+// Adiciona ouvinte de evento para monitorar mudanças nos checkboxes de gêneros
+const genreCheckboxes = document.querySelectorAll('#genero input[type="checkbox"]');
+genreCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', updateGenres); // Detecta quando o checkbox de gêneros for alterado
+});
+
+// Adiciona ouvinte de evento para monitorar mudanças no input de gêneros
+document.getElementById('genres').addEventListener('input', checkGenres);
+
+
+
+
+window.addEventListener('load', () => {
+    checkCategories(); // Atualiza categorias
+    checkGenres(); 
+   
+});
+
+
+
+
+
+
+
+    
---
+++ Main/Gerador/js/script.js
@@ -0,0 +1,227 @@
+
+
+// home
+
+const API_KEY = '2018cef294a4077a7c4ab5a06a153826';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+let currentMoviePage = 1; // Página atual de filmes
+let currentTVPage = 1; // Página atual de séries
+let totalMoviePages = 0; // Total de páginas de filmes
+let totalTVPages = 0; // Total de páginas de séries
+let totalMovies = 0; // Total de postagens de filmes
+let totalTVShows = 0; // Total de postagens de séries
+let isSearching = false; // Flag para saber se está pesquisando
+
+// Idiomas permitidos
+const allowedLanguages = ['pt', 'en', 'ja', 'ko', 'fr', 'es', 'zh'];
+
+async function fetchPopularMovies(page = 1) {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=pt-BR&page=${page}`);
+    const data = await response.json();
+    totalMovies = data.total_results; // Atualiza total de postagens de filmes
+    totalMoviePages = Math.ceil(totalMovies / 20); // Total de páginas
+    return data.results.filter(movie => allowedLanguages.includes(movie.original_language) && movie.poster_path); // Filtra por idiomas permitidos e verifica se há imagem
+}
+
+async function fetchPopularTVShows(page = 1) {
+    const response = await fetch(`${BASE_URL}/tv/popular?api_key=${API_KEY}&language=pt-BR&page=${page}`);
+    const data = await response.json();
+    totalTVShows = data.total_results; // Atualiza total de postagens de séries
+    totalTVPages = Math.ceil(totalTVShows / 20); // Total de páginas
+    return data.results.filter(tvShow => allowedLanguages.includes(tvShow.original_language) && tvShow.poster_path); // Filtra por idiomas permitidos e verifica se há imagem
+}
+
+async function fetchMovies(query, page = 1) {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&page=${page}`);
+    const data = await response.json();
+    totalMovies = data.total_results; // Atualiza total de postagens de filmes
+    totalMoviePages = Math.ceil(totalMovies / 20); // Recalcula o total de páginas baseado na pesquisa
+    return data.results.filter(movie => allowedLanguages.includes(movie.original_language) && movie.poster_path); // Filtra por idiomas permitidos e verifica se há imagem
+}
+
+async function fetchTVShows(query, page = 1) {
+    const response = await fetch(`${BASE_URL}/search/tv?api_key=${API_KEY}&query=${query}&page=${page}`);
+    const data = await response.json();
+    totalTVShows = data.total_results; // Atualiza total de postagens de séries
+    totalTVPages = Math.ceil(totalTVShows / 20); // Recalcula o total de páginas baseado na pesquisa
+    return data.results.filter(tvShow => allowedLanguages.includes(tvShow.original_language) && tvShow.poster_path); // Filtra por idiomas permitidos e verifica se há imagem
+}
+
+function displayResults(results, containerId, isMovie = true) {
+    const container = document.getElementById(containerId);
+    if (results.length === 0) {
+        container.innerHTML = '<p>Não há postagens disponíveis.</p>'; // Adiciona aviso se não houver resultados
+        return;
+    }
+    
+    container.innerHTML = results.map(result => {
+        let category = '';
+        let categoryClass = ''; // Variável para a classe do card
+
+        if (!isMovie) { // Apenas para séries
+            const originalLanguage = result.original_language;
+            const isAnimation = result.genre_ids.includes(16); // Verifica se é um anime
+
+            // Atribui a categoria e a classe com base no idioma
+            if (isAnimation) {
+                if (originalLanguage === 'ja' || originalLanguage === 'zh') {
+                    category = '#Anime';
+                    categoryClass = 'anime'; // Classe para anime
+                } else if (['pt', 'en'].includes(originalLanguage)) {
+                    category = '#Animação';
+                    categoryClass = 'animação'; // Classe para animação
+                } else {
+                    category = '#Animação'; // Outros idiomas
+                    categoryClass = 'animação'; // Classe para animação
+                }
+            } else {
+                // Verifica se é uma série coreana que não é animação
+                if (originalLanguage === 'ko') {
+                    category = '#Dorama';
+                    categoryClass = 'dorama'; // Classe para dorama
+                } else {
+                    category = '#Série'; // Para outras séries
+                    categoryClass = 'serie'; // Classe para série
+                }
+            }
+        } else { // Para filmes
+            const originalLanguage = result.original_language;
+            const isAnimation = result.genre_ids.includes(16); // Verifica se é uma animação
+
+            if (isAnimation) {
+                if (originalLanguage === 'ja') {
+                    category = 'Filme - Anime'; // Para filmes do Japão (anime)
+                    categoryClass = 'filme-anime'; // Classe para filme-anime
+                } else {
+                    category = 'Filme - Animação'; // Para outros filmes de animação
+                    categoryClass = 'filme-animacao'; // Classe para filme-animação
+                }
+            } else {
+                category = 'Filme'; // Para filmes comuns
+                categoryClass = 'filme'; // Classe para filme
+            }
+        }
+
+        return `
+            <a href="${isMovie ? 'filme.html' : 'serie.html'}?data-id=${result.id}&${categoryClass}" class="card ${categoryClass}">
+                <img src="https://image.tmdb.org/t/p/w500${result.poster_path}">
+                <h3>${result.title || result.name}</h3>
+                <h8 type="${category}">${isMovie ? category.replace('-', ' - ') : ''}</h8>
+                <p>${result.release_date || result.first_air_date}</p>
+            </a>
+        `;
+    }).join('');
+}
+
+function openDetailPage(card) {
+    const id = card.getAttribute('data-id'); // Obtém o data-id do card
+    const isMovie = card.classList.contains('filme'); // Verifica se é um filme
+    const detailPage = isMovie ? 'filme.html' : 'serie.html'; // Define a página de detalhes com base no tipo
+    window.location.href = `${detailPage}?data-id=${id}`; // Passa o data-id na URL
+}
+
+
+function updatePageInfo() {
+    document.getElementById('current-movie-page').textContent = `Página ${currentMoviePage} de ${totalMoviePages}`;
+    document.getElementById('current-tv-page').textContent = `Página ${currentTVPage} de ${totalTVPages}`;
+    document.getElementById('load-prev-movies').disabled = currentMoviePage === 1; // Desabilitar botão se for a primeira página
+    document.getElementById('load-prev-tv').disabled = currentTVPage === 1; // Desabilitar botão se for a primeira página
+    document.getElementById('load-more-movies').disabled = currentMoviePage >= totalMoviePages; // Desabilitar se não houver próxima página
+    document.getElementById('load-more-tv').disabled = currentTVPage >= totalTVPages; // Desabilitar se não houver próxima página
+    document.getElementById('total-movies').textContent = `(${totalMovies})`; // Atualiza total de filmes
+    document.getElementById('total-tv').textContent = `(${totalTVShows})`; // Atualiza total de séries
+}
+
+async function loadInitialContent() {
+    const movies = await fetchPopularMovies(currentMoviePage);
+    const tvShows = await fetchPopularTVShows(currentTVPage);
+    
+    displayResults(movies, 'movie-results', true);
+    displayResults(tvShows, 'tv-results', false);
+    updatePageInfo(); // Atualiza as informações da página
+}
+
+document.getElementById('search').addEventListener('input', async (event) => {
+    const query = event.target.value.trim();
+    
+    if (query) {
+        isSearching = true; // Define que está pesquisando
+        currentMoviePage = 1; // Reinicia a página ao buscar
+        currentTVPage = 1; // Reinicia a página ao buscar
+        const movies = await fetchMovies(query, currentMoviePage);
+        const tvShows = await fetchTVShows(query, currentTVPage);
+        
+        displayResults(movies, 'movie-results', true);
+        displayResults(tvShows, 'tv-results', false);
+        updatePageInfo(); // Atualiza as informações da página
+    } else {
+        isSearching = false; // Define que não está mais pesquisando
+        currentMoviePage = 1; // Reinicia a página ao buscar
+        currentTVPage = 1; // Reinicia a página ao buscar
+        loadInitialContent(); // Carrega o conteúdo inicial
+    }
+});
+
+// Navegação de páginas para filmes
+document.getElementById('load-more-movies').addEventListener('click', async () => {
+    if (isSearching) {
+        currentMoviePage++;
+        const movies = await fetchMovies(document.getElementById('search').value.trim(), currentMoviePage);
+        displayResults(movies, 'movie-results', true);
+    } else {
+        currentMoviePage++;
+        const movies = await fetchPopularMovies(currentMoviePage);
+        displayResults(movies, 'movie-results', true);
+    }
+    updatePageInfo(); // Atualiza as informações da página
+});
+
+document.getElementById('load-prev-movies').addEventListener('click', async () => {
+    if (currentMoviePage > 1) {
+        currentMoviePage--;
+        if (isSearching) {
+            const movies = await fetchMovies(document.getElementById('search').value.trim(), currentMoviePage);
+            displayResults(movies, 'movie-results', true);
+        } else {
+            const movies = await fetchPopularMovies(currentMoviePage);
+            displayResults(movies, 'movie-results', true);
+        }
+        updatePageInfo(); // Atualiza as informações da página
+    }
+});
+
+// Navegação de páginas para séries
+document.getElementById('load-more-tv').addEventListener('click', async () => {
+    if (isSearching) {
+        currentTVPage++;
+        const tvShows = await fetchTVShows(document.getElementById('search').value.trim(), currentTVPage);
+        displayResults(tvShows, 'tv-results', false);
+    } else {
+        currentTVPage++;
+        const tvShows = await fetchPopularTVShows(currentTVPage);
+        displayResults(tvShows, 'tv-results', false);
+    }
+    updatePageInfo(); // Atualiza as informações da página
+});
+
+document.getElementById('load-prev-tv').addEventListener('click', async () => {
+    if (currentTVPage > 1) {
+        currentTVPage--;
+        if (isSearching) {
+            const tvShows = await fetchTVShows(document.getElementById('search').value.trim(), currentTVPage);
+            displayResults(tvShows, 'tv-results', false);
+        } else {
+            const tvShows = await fetchPopularTVShows(currentTVPage);
+            displayResults(tvShows, 'tv-results', false);
+        }
+        updatePageInfo(); // Atualiza as informações da página
+    }
+});
+
+// Carrega o conteúdo inicial ao iniciar
+loadInitialContent();
+verificaTrechosURL();
+
+
+
