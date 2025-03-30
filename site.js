1229edbe9a15ccf234c671069680a4b65c146a10
controls player <130595852+site-codes@users.noreply.github.com>
2025-03-30 17:00:50 -0300

    Update site.js

--- site.js
+++ site.js
@@ -4,4 +4,3 @@ document.write(`<script src="https://instinto-play.vercel.app/Geral/geralJs/scri
 document.write(`<script src="https://instinto-play.vercel.app/Geral/geralJs/script04.js"><\/script>`);
 document.write(`<script src="https://instinto-play.vercel.app/Geral/geralJs/script05.js"><\/script>`);
 document.write(`<script src="https://instinto-play.vercel.app/Geral/geralJs/script2.js"><\/script>`);
-document.write(`<script src="https://instinto-play.vercel.app/Geral/geralJs/script.js"><\/script>`);
