0f7b1eda9163ce45a19f1ac4756b02cd9341566e
controls player <130595852+site-codes@users.noreply.github.com>
2025-03-30 23:09:15 -0300

    Delete site.js

--- site.js
+++
@@ -1,7 +0,0 @@
-document.write(`<script src="https://instinto-play.vercel.app/Geral/geralJs/script01.js"><\/script>`);
-document.write(`<script src="https://instinto-play.vercel.app/Geral/geralJs/script02.js"><\/script>`);
-document.write(`<script src="https://instinto-play.vercel.app/Geral/geralJs/script03.js"><\/script>`);
-document.write(`<script src="https://instinto-play.vercel.app/Geral/geralJs/script04.js"><\/script>`);
-document.write(`<script src="https://instinto-play.vercel.app/Geral/geralJs/script05.js"><\/script>`);
-document.write(`<script src="https://instinto-play.vercel.app/Geral/geralJs/script2.js"><\/script>`);
-document.write(`<script src="https://instinto-play.vercel.app/Geral/geralJs/script.js"><\/script>`);
