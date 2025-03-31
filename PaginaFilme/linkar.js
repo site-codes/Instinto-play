bdd4745647523c60a1d4df63af3704e74cdac0d2
controls player <130595852+site-codes@users.noreply.github.com>
2025-03-30 23:07:25 -0300

    Delete PaginaFilme directory

--- PaginaFilme/linkar.js
+++
@@ -1,7 +0,0 @@
-  
-  document.write('<script src="https://instinto-play.vercel.app/PaginaEpisodio/Player/geral.js"><\/script>');
-  document.write('<link rel="stylesheet" href="https://instinto-play.vercel.app/PaginaEpisodio/Player/geral.css"/>');
-    
-document.write('<script src="https://instinto-play.vercel.app/PaginaFilme/script.js"><\/script>');
-document.write('<link rel="stylesheet" href="https://instinto-play.vercel.app/PaginaFilme/style.css"/>');
-document.write('<link rel="stylesheet" href="https://instinto-play.vercel.app/PaginaAnime/style.css"/>');
