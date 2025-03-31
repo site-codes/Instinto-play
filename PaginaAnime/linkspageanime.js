e4201d14a8d1745a7eedbb13a2a4da8c424041d2
controls player <130595852+site-codes@users.noreply.github.com>
2025-03-30 23:07:46 -0300

    Delete PaginaAnime directory

--- PaginaAnime/linkspageanime.js
+++
@@ -1,5 +0,0 @@
-document.write(`
-   <link rel="stylesheet" href="https://instinto-play.vercel.app/PaginaAnime/style.css">
-   <script src="https://instinto-play.vercel.app/PaginaAnime/script.js"><\/script>
-   <script src="https://instinto-play.vercel.app/PaginaAnime/listaeps.js"><\/script>
-`);
